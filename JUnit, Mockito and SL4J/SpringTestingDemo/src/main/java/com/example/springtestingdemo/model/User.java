864b92975386cd41076f047b8fa6a940d1f9819e
Monika Saravanan <147172253+Moni-saravanan@users.noreply.github.com>
2025-06-29 21:16:08 +0530

    Update User.java

--- JUnit, Mockito and SL4J/SpringTestingDemo/src/main/java/com/example/springtestingdemo/model/User.java
+++ JUnit, Mockito and SL4J/SpringTestingDemo/src/main/java/com/example/springtestingdemo/model/User.java
@@ -9,7 +9,6 @@ public class User {
     private Long id;
     private String name;
 
-    // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
