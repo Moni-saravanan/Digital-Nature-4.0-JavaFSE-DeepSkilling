aa6e7d3514383d982c14d94a5cbf02abe588c5e6
Monika Saravanan <147172253+Moni-saravanan@users.noreply.github.com>
2025-06-29 21:11:54 +0530

    Update PerformanceTesterTest.java

--- JUnit, Mockito and SL4J/JUnitAdvancedTestingDemo/src/test/java/org/example/PerformanceTesterTest.java
+++ JUnit, Mockito and SL4J/JUnitAdvancedTestingDemo/src/test/java/org/example/PerformanceTesterTest.java
@@ -11,6 +11,6 @@ public class PerformanceTesterTest {
     @Timeout(value = 1, unit = TimeUnit.SECONDS)
     void testPerformTaskTimeout() throws InterruptedException {
         PerformanceTester tester = new PerformanceTester();
-        tester.performTask();  // This should not exceed 1 second
+        tester.performTask();
     }
 }
